f76f86fe17a20aae984e3264d1604677da90bf09
DValenciaD <50994288+DValenciaD@users.noreply.github.com>
2019-12-06 21:04:59 -0600

    Update EnemigoPer.cs

--- EnemigoPer.cs
+++ EnemigoPer.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 // Daniela Itzel Valencia Durán
 // Programación Orientada a Objetos
 // Josue Israel Rivas Díaz
-// 
+// Persigue al jugador
 
 public class EnemigoPer : Persecucion //Hereda a EnemigoPer
 {
     // Start is called before the first frame update
     void Start()
     {
-        playerPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        playerPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>(); //La posición del jugador es igual al objeto con la etiqueta "Player"
     }
 
     // Update is called once per frame
     void Update()
     {
-        Perseguir(playerPosition, this.gameObject.transform); //
+        Perseguir(playerPosition, this.gameObject.transform); //Se dirije a la posicion del jugador
     }
 }
