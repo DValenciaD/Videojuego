d90313d79c4bb9b64e904a3cb48a8a752d337b42
DValenciaD <50994288+DValenciaD@users.noreply.github.com>
2019-12-06 22:21:35 -0600

    Add files via upload

---
+++ Persecucion.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Daniela Itzel Valencia Durán
+// Programación Orientada a Objetos
+// Josue Israel Rivas Díaz
+//Delimita la posición del objeto con el tag "Player", para que el enemigo transforme su posición hasta el jugador 
+
+public class Persecucion : MonoBehaviour
+{
+    public Transform playerPosition; 
+    public float velocidadEnemigo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Perseguir(playerPosition, this.gameObject.tranform);
+    }
+
+    public void Perseguir(Transform player, Transform enemigo) //Seleciona a "player" como su objetivo y hace que el enemigo se dirija hacia el
+    {
+        Vector3 distancia = player.position - enemigo.position;
+
+        enemigo.LookAt(distancia);
+        enemigo.Translate(distancia.normalized * velocidadEnemigo * Time.deltaTime);
+    }
+}
---
+++ Skeleton.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : Agente
+{
+    // Daniela Itzel Valencia Durán
+    // Programación Orientada a Objetos
+    // Josue Israel Rivas Díaz
+    // Ejecuta las animaciones del esqueleto y hace que siga al objeto 
+
+    public string Id;
+    public string nombre;
+    public int vida;
+    public int magia;
+    public float _velocidadAgente;
+
+    Animator anim;
+    EnemigoB enemigoB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        VelocidadAgente = velocidad;
+        enemigoB = FindObjectOfType<EnemigoB>();
+
+        BusquedaEnemigo(Id);
+
+        this.anim = GetComponent<Animator>();
+        destino = GameObject.Find("Destino").GetComponent<Transform>();
+        objetivo = GameObject.Find("Destino").GetComponent<Transform>();
+    }
+
+    private void BusquedaEnemigo(string id)
+    {
+        for (int i = 0; i < enemigoB.enemigo.Count; i++) //De la base de datos del enemigo, lee los objetos de la lista
+        {
+            if (id == enemigoB.enemigo[i].nombre) //Si se encuentra el dato del bloque, eonces a la variable se le agregan los valores siguientes
+            {
+                nombre = enemigoB.enemigo[i].nombre;
+                vida = enemigoB.enemigo[i].vida;
+                magia = enemigoB.enemigo[i].magia;
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MedirDistanciaBool())
+        {
+            ConfigurarDestino(destino);
+
+            if (MedirDistanciaFloat() <= freno)
+            {
+                Debug.Log("Toma");
+                anim.SetBool("Atque", true);
+            }
+            else
+            {
+                anim.SetBool("Ataque", false);
+            }
+        }
+        else if (!MedirDistanciaBool()) //Si "MedirDistanciaBool" es diferente
+        {
+            VelocidadAgente = 0;
+        }
+    }
+
+}
